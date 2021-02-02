00f5ba57cba24558f2785224e69950d4fb1c72ea
vivekvaradarajan1980 <varadarajan.vivek@gmail.com>
2021-02-02 15:26:17 -0600

    CLI commit

---
+++ src/main/java/com/galvanize/SomeProgram.java
@@ -0,0 +1,9 @@
+package com.galvanize;
+
+public class SomeProgram {
+    public static void main(String[] args) {
+
+   if(args.length<1)
+   System.out.println("enter at least 1 argument");
+    }
+}
